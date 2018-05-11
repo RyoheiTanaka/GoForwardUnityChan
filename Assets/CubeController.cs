84314576acf292ed175230c76578d08d2aa2bc2b
RyoheiTanaka <ryohei632@gmail.com>
2018-05-11 21:42:48 +0900

    Lesson7課題 接触時の効果音
    ブロックが地面かブロックに接触した際に効果音を鳴らす

---
+++ Assets/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	// キューブ接触時のSEを入れる
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// キューブを移動させる
+		transform.Translate(this.speed, 0, 0);
+
+		// 画面外に出たら破棄する
+		if(transform.position.x < this.deadLine) {
+			Destroy(gameObject);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if(other.gameObject.tag == "GroundTag" || other.gameObject.tag == "CubeTag") {
+			audioSource.Play();
+		}
+	}
+}
